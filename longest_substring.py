140194506ae9c0e83ab43316c21f2f06f6d851cf
bednie <bednie@msudenver.edu>
2023-06-05 17:57:59 -0600

    "Added sliding window soln for Longest Substring

--- longest_substring.py
+++ longest_substring.py
@@ -1,5 +1,6 @@
 # 3. Longest Substring Without Repeating Characters
 
+# brute force
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if len(s) == 1:
@@ -20,3 +21,21 @@ class Solution:
                     substring.append(next[j])
 
         return max(len(longest),len(substring))
+    
+# sliding window 
+class Solution2:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        seen = {}
+        left = 0
+        max_len = 0
+
+        for right,char in enumerate(s):
+            if char in seen and left <= seen[char]:
+                left = seen[char] + 1 
+                seen.pop(char)
+
+            seen[char] = right 
+            max_len = max(max_len,(right-left+1)) 
+
+        return max_len
