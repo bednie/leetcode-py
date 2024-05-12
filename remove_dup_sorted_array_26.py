2673835495b0a6f2fac145d3cc3a277a9b85111e
Blair Ednie <bednie@msudenver.edu>
2024-05-12 15:38:07 -0600

    Change ne to gt

--- remove_dup_sorted_array_26.py
+++ remove_dup_sorted_array_26.py
@@ -5,9 +5,9 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         copy_to = 1
         for next in range(1, len(nums)):
-            if nums[next] != nums[next - 1]:
+            if nums[next] > nums[next-1]:
                 nums[copy_to] = nums[next]
-                copy_to += 1
+                copy_to += 1   
         return copy_to
 
 
