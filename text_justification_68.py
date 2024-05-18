64943e0c32021d325cba2b1300bc7caaa4d04518
Blair Ednie <bednie@msudenver.edu>
2024-05-17 23:49:47 -0600

    Solve 68

--- text_justification_68.py
+++ text_justification_68.py
@@ -4,47 +4,74 @@ from typing import List
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
         def justify_line(line: List[str]) -> str:
-            width = sum([len(word) for word in line])
-            while width < maxWidth:
-                for i in line[: len(line) - 1]:
-                    i += " "
-                    width += 1
+            if len(line) == 1:
+                if line[0] == maxWidth:
+                    return line[0]
+                width = len(line[0])
+                line[0] = line[0] + (" " * (maxWidth - width))
+                return line[0]
 
-            justified = ""
+            width = 0
             for i in line:
-                justified += justified + i
+                width += len(i)
 
-            return justified
+            while width < maxWidth:
+                for i in range(len(line) - 1):
+                    line[i] += " "
+                    width += 1
+                    if width == maxWidth:
+                        break
+
+            justified_line = ""
+            for i in line:
+                justified_line += i
+
+            return justified_line
+
+        def left_justify(line: List[str]) -> str:
+            justified_line = " ".join(line)
+            width = len(justified_line)
+            justified_line = justified_line + (" " * (maxWidth - width))
+
+            return justified_line
 
         chunks = []
         chunk = []
         while words:
             word = words.pop(0)
+
             if len(" ".join(chunk)) + 1 + len(word) <= maxWidth:
                 chunk.append(word)
+
+            elif len(word) == maxWidth:
+                if chunk != []:
+                    chunks.append(chunk[:])
+                    chunk.clear()
+                chunks.append([word])
 
             else:
                 chunks.append(chunk[:])
                 chunk.clear()
                 chunk.append(word)
 
-            if not words:
+            if not words and chunk != []:
                 chunks.append(chunk[:])
 
-        print(chunks)
-
         message = []
-        for i in chunks[: len(chunks) - 1]:
-            message.append(justify_line(i))
+        for c in range(len(chunks) - 1):
+            message.append(justify_line(chunks[c]))
 
-        # append last chunk with right padding
-        message.append(" ".join(chunks[-1]))
+        message.append(left_justify(chunks[-1]))
 
-        print(message)
-
-        return [0]
+        return message
 
 
 # test
 sol = Solution()
-print(sol.fullJustify(["What", "must", "be", "acknowledgment", "shall", "be"]))
+assert sol.fullJustify(["What", "must", "be", "shall", "be."], 5) == [
+    "What ",
+    "must ",
+    "be   ",
+    "shall",
+    "be.  ",
+], "Wrong"
