e2b855c3985611203f424962e66f79c53c94f456
Blair Ednie <bednie@msudenver.edu>
2024-05-18 12:14:22 -0600

    Solve 151

--- reverse_words_str_151.py
+++ reverse_words_str_151.py
@@ -1,6 +1,5 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s = s.strip().split(" ")
+        s = s.split(" ")
         s = " ".join(s).split()
-
         return " ".join(reversed(s))
