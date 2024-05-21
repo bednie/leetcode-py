aba2883e2f521325862c166165dfa60c346688fb
Blair Ednie <bednie@msudenver.edu>
2024-05-21 14:31:23 -0600

    Clean up

--- contains_dup_ii_219.py
+++ contains_dup_ii_219.py
@@ -1,36 +1,26 @@
-from typing import List
-
+from collections import defaultdict
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        if k == 0 or len(nums) == 1:
-            return False
-
         if k >= len(nums) - 1:
             k = len(nums) - 1
 
-        # frequency of elements in window length k
-        freqs = {}
+        # frequency of elements in k window
+        freqs = defaultdict(int)
         start, end = 0, k + 1
 
         # initialize
         for i in range(k + 1):
-            if nums[i] not in freqs:
-                freqs[nums[i]] = 1
-            else:
+            freqs[nums[i]] += 1
+            if freqs[nums[i]] == 2:
                 return True
 
         # maintain freqs
         while end < len(nums):
             freqs[nums[start]] -= 1
+            freqs[nums[end]] += 1
 
-            if nums[end] not in freqs:
-                freqs[nums[end]] = 1
-
-            elif freqs[nums[end]] >= 0:
-                freqs[nums[end]] += 1
-
-            if freqs[nums[end]] >= 2:
+            if freqs[nums[end]] == 2:
                 return True
 
             start += 1
