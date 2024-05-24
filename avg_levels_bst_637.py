b13e2f60584ee1b94dbeb32afe5128cfd79658f3
Blair Ednie <bednie@msudenver.edu>
2024-05-24 13:33:19 -0600

    Remove len call

--- avg_levels_bst_637.py
+++ avg_levels_bst_637.py
@@ -15,20 +15,25 @@ class Solution:
 
         # bfs
         queue = [root]
+        next_length = 1
+        temp = []
         while queue:
-            length = len(queue)
-            temp = []
+            length = next_length
+            next_length = 0
             level_sum = 0
 
             while queue:
-                node = queue.pop(0)
+                node = queue.pop()
                 level_sum += node.val
                 if node.left:
                     temp.append(node.left)
+                    next_length += 1
                 if node.right:
                     temp.append(node.right)
+                    next_length += 1
 
             averages.append(level_sum / length)
             queue += temp
+            temp.clear()
 
         return averages
