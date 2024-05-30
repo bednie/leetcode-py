4c11a3b135ef34a702672c7aa47a630aa987ce5e
Blair Ednie <bednie@msudenver.edu>
2024-05-29 20:11:02 -0600

    Solve 15

--- three_sum_15.py
+++ three_sum_15.py
@@ -6,36 +6,24 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         ans = set()
         map = defaultdict(set)
-        unique = set()
         nums.sort()
-        start, end, mid = 0, len(nums), len(nums) // 2
 
         for i, j in enumerate(nums):
             map[j].add(i)
-            unique.add(j)
 
-        unique = set(tuple([i for i in sorted(list(unique))]))
-
-        for i in range(mid, end - 1):
-            for j in range(i + 1, end):
-                if -(nums[i] + nums[j]) in map and map[-(nums[i] + nums[j])].difference(
-                    {i, j}
-                ):
-                    x = tuple(sorted([nums[j], nums[i], (-(nums[i] + nums[j]))]))
-                    ans.add(x)
-                    if not unique.difference(set(x)):
-                        return list(ans)
-
-        for i in range(mid, start, -1):
-            if ans.difference(unique) == {}:
-                break
-            for j in range(i - 1, start - 1, -1):
-                if -(nums[i] + nums[j]) in map and map[-(nums[i] + nums[j])].difference(
-                    {i, j}
-                ):
-                    x = tuple(sorted([nums[j], nums[i], (-(nums[i] + nums[j]))]))
-                    ans.add(x)
-                    if not unique.difference(set(x)):
-                        return list(ans)
+        for i in range(len(nums) - 2):
+            start, end = i + 1, len(nums) - 1
+            while start < end:
+                if nums[start] + nums[end] + nums[i] > 0:
+                    end -= 1
+                elif nums[start] + nums[end] + nums[i] < 0:
+                    start += 1
+                else:
+                    if -(nums[start] + nums[end]) in map and (
+                        i != start and i != end and start != end
+                    ):
+                        ans.add(tuple([nums[start], nums[end], nums[i]]))
+                    start += 1
+                    end -= 1
 
         return list(ans)
