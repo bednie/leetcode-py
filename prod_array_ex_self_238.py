9fd8cacd9d8753edf9d1a234eae6095381823ce6
Blair Ednie <bednie@msudenver.edu>
2024-06-01 18:21:19 -0600

    Solve 238

---
+++ prod_array_ex_self_238.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = {-1: 1}
+        for i in range(0, len(nums)):
+            prefix[i] = nums[i] * prefix[i - 1]
+
+        suffix = {len(nums): 1}
+        for i in range(len(nums) - 1, -1, -1):
+            suffix[i] = nums[i] * suffix[i + 1]
+
+        ans = [0] * len(nums)
+        for i in range(len(nums)):
+            ans[i] = prefix[i - 1] * suffix[i + 1]
+
+        return ans
