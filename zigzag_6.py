f95894ccd7a082404b7c0e5f0fcf5030b686679a
Blair Ednie <bednie@msudenver.edu>
2024-06-05 12:00:25 -0600

    Update solution

--- zigzag_6.py
+++ zigzag_6.py
@@ -1,25 +1,14 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if len(s) == 1:
-            return s
-
-        if numRows == 1:
+        if len(s) == 1 or numRows == 1:
             return s
 
         ans = ["" for i in range(numRows)]
-        row = 0
-        i = 0
+        row, i, zigzag = 0, 0, 1
         while i < len(s):
-            if row == 0:
-                while row < numRows - 1 and i < len(s):
-                    ans[row] += s[i]
-                    row += 1
-                    i += 1
-
-            if row == numRows - 1:
-                while row > 0 and i < len(s):
-                    ans[row] += s[i]
-                    row -= 1
-                    i += 1
-
+            ans[row] += s[i]
+            row += zigzag
+            i += 1
+            if row == numRows - 1 or row == 0:
+                zigzag *= -1
         return "".join(ans)
