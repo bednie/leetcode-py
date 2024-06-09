f371b5a42884b98856423143aee076473150ba60
Blair Ednie <bednie@msudenver.edu>
2024-06-09 11:48:24 -0600

    Solve 162

--- peak_elem_162.py
+++ peak_elem_162.py
@@ -8,11 +8,9 @@ class Solution:
         while i < j:
             mid = (j + i) // 2
 
-            if nums[mid - 1] <= nums[mid] > nums[mid + 1]:
-                return mid
-
-            if nums[mid] <= nums[mid + 1]:
+            if nums[mid] < nums[mid + 1]:
                 i = mid + 1
             else:
                 j = mid
         return i
+
