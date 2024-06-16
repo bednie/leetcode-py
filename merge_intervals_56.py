4a70df2e448afc5ce4a4dc871dc3f7031dbf1338
Blair Ednie <bednie@msudenver.edu>
2024-06-16 11:37:33 -0600

    Solve 56

--- merge_intervals_56.py
+++ merge_intervals_56.py
@@ -4,17 +4,13 @@ from typing import List
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        result = []
         i = 0
         while i < len(intervals) - 1:
-            if intervals[i][1] < intervals[i+1][0]:
-                result.append(intervals[i])
+            if intervals[i][1] < intervals[i + 1][0]:
                 i += 1
 
             else:
-                r = sum([intervals[i], intervals.pop(i+1)], [])
-                print(r)
+                r = sum([intervals[i], intervals.pop(i + 1)], [])
                 intervals[i] = [min(r), max(r)]
 
-        result.extend(intervals[i:])
-        return result
+        return intervals
