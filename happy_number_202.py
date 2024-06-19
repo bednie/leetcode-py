15ea28c4abb1c2ce662dab50e713098ac63166f0
Blair Ednie <bednie@msudenver.edu>
2024-06-19 10:21:46 -0600

    Solve 202

---
+++ happy_number_202.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        nn = n
+        m = 0 # sum of squares
+        while nn > 0:
+            d = nn % 10
+            m += d * d
+            nn -= d
+            nn //= 10
+
+        if m == 1:
+            return True
+        
+        elif m == 4 or m == n:
+            return False
+
+        else:
+            return self.isHappy(m)
