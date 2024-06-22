ebe1e1a44ca045a10570ba6e6890af9a368c8129
Blair Ednie <bednie@msudenver.edu>
2024-06-22 14:19:17 -0600

    Work on 71

--- simplify_path_71.py
+++ simplify_path_71.py
@@ -2,11 +2,11 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         ans = ""
         p = path.split("/")
-        p = [i for i in p if i]
+        p = [i for i in p if i and i != '.']
 
         i = 0
         while i < len(p):
-            if p[i] == ".." or p[i] == ".":
+            if p[i] == "..":
                 pass
             elif i + 1 < len(p) and p[i+1] == "..":
                 i += 2
@@ -14,7 +14,7 @@ class Solution:
             else:
                 ans += "/"
                 ans += p[i]
-                
+
             i += 1
 
         if ans:
