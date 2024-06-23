d9eaa25f03f78379ed7feb2aa8cce3e9937f6acf
Blair Ednie <bednie@msudenver.edu>
2024-06-23 12:13:53 -0600

    Solve 128

---
+++ longest_consec_seq_128.py
@@ -0,0 +1,45 @@
+from typing import List
+
+
+class Node: 
+    def __init__(self):
+        self.low = None
+        self.high = None
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nodes = []
+        seq = {}
+        max_len = 0
+        for i in nums:
+            if i in seq:
+                pass
+
+            elif i-1 in seq and i+1 not in seq:
+                seq[i] = seq[i-1]
+                seq[i].high = i
+
+            elif i-1 not in seq and i+1 in seq:
+                seq[i] = seq[i+1]
+                seq[i].low = i
+
+
+            elif i-1 in seq and i+1 in seq:
+                seq[i] = seq[i-1]
+                seq[i].high = seq[i+1].high
+                nodes.remove(seq[i+1])
+                seq[seq[i].high] = seq[i]
+
+            else:
+                node = Node()
+                node.high = i
+                node.low = i
+                nodes.append(node)
+                seq[i] = node
+
+            max_len = max(max_len, seq[i].high - seq[i].low + 1)
+
+        return max_len
+
+
