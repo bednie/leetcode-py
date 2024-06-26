8fbea7787b003aa4f6b8f1f393d4a6c7a6a3b767
Blair Ednie <bednie@msudenver.edu>
2024-06-26 14:05:34 -0600

    Work on 101

---
+++ symmetric_tree_101.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        deque = [root]
+        while deque:
+            left, right = 0, len(deque) - 1
+            while left <= right:
+                if deque[left] and not deque[right] or not deque[left] and deque[right]:
+                    return False
+
+                elif (
+                    deque[left] and deque[right] and deque[left].val != deque[right].val
+                ):
+                    return False
+
+                else:
+                    left += 1
+                    right -= 1
+
+            temp = []
+            while deque:
+                node = deque.pop(0)
+                if node:
+                    temp.append(node.left)
+                    temp.append(node.right)
+
+            deque.extend(temp)
+
+        return True
