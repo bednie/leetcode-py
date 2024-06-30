83e9e20a8c05f503e177493a6598f8303fa65caa
Blair Ednie <bednie@msudenver.edu>
2024-06-29 21:15:02 -0600

    Work on 150

---
+++ eval_rpn_150.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for i in tokens:
+            if i == '+':
+                stack.append(stack.pop() + stack.pop())
+
+            elif i == '*':
+                stack.append(stack.pop() * stack.pop())
+
+            elif i == '-':
+                arg1 = stack.pop()
+                arg2 = stack.pop()
+                stack.append(arg2 - arg1)
+
+            elif i == '/':
+                arg1 = stack.pop()
+                arg2 = stack.pop()
+                stack.append(int(arg2 / arg1))
+
+            else:
+                stack.append(int(i))
+
+            print(stack)
+
+        
+        return stack.pop()
