6b59cdbade902c512a04f89be56fa2e50ff8b3ce
Blair Ednie <bednie@msudenver.edu>
2024-07-01 10:58:41 -0600

    Work on 452

---
+++ arrows_452.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        #points.sort()
+        arrows = []
+        for p in points:
+            if not arrows:
+                arrows.append(p)
+                continue
+
+            if p[0] <= arrows[-1][1]:
+                arrows[-1][0] = max(arrows[-1][0], p[0])
+                arrows[-1][1] = min(arrows[-1][1], p[1])
+            else:
+                arrows.append(p)
+
+        #print(arrows)
+        return len(arrows)
