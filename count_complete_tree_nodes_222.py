5b54acb3cfb676a3a6d2956063227f84251bbc5c
Blair Ednie <bednie@msudenver.edu>
2024-07-08 18:01:12 -0600

    Solve 222

--- count_complete_tree_nodes_222.py
+++ count_complete_tree_nodes_222.py
@@ -11,26 +11,23 @@ class TreeNode:
 
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        depth_cache = {}
-
-        def depth(node: Optional[TreeNode], right: bool) -> int:
+        def depth(node: Optional[TreeNode]) -> int:
             if not node:
                 return 0
-            key = (id(node), right)
-            if key not in depth_cache:
-                if right:
-                    depth_cache[key] = 1 + depth(node.right, True)
-                else:
-                    depth_cache[key] = 1 + depth(node.left, False)
-            return depth_cache[key]
+            key = id(node)
+            if key not in memo:
+                memo[key] = 1 + depth(node.left)
+            return memo[key]
+
+        memo = {}
 
         if not root:
             return 0
 
-        left, right = depth(root, False), depth(root, True)
+        left, right = depth(root.left), depth(root.right)
 
         if left == right:
-            return (2**left) - 1
+            return (2**left) + self.countNodes(root.right)
 
         else:
-            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+            return (2**right) + self.countNodes(root.left)
