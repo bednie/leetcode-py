ba65ab29a33098865e4f882f3116fa035b2beb6d
Blair Ednie <bednie@msudenver.edu>
2024-07-12 18:10:23 -0600

    Solve 322

--- coin_change_322.py
+++ coin_change_322.py
@@ -1,11 +1,13 @@
 from typing import List
 
 
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         def dp(amount: int) -> int:
             for coin in coins[::-1]:
-                memo[amount] = min(memo[amount], memo[amount - coin] + 1)
+                if coin <= amount:
+                    memo[amount] = min(memo[amount], memo[amount - coin] + 1)
 
         memo = [amount + 1] * (amount + 1)
         memo[0] = 0
