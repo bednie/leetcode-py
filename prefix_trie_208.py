4451f51c4bec8327a31e0b9f00e301861752c35f
Blair Ednie <bednie@msudenver.edu>
2024-08-01 20:13:10 -0600

    Solve 208

--- prefix_trie_208.py
+++ prefix_trie_208.py
@@ -30,10 +30,3 @@ class Trie:
             children = children[index]
 
         return True
-
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
