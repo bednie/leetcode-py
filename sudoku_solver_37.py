9102d35cc91bc2cc9aed4ee557fa43744403c810
Blair Ednie <bednie@msudenver.edu>
2024-08-26 12:18:10 -0600

    Solve 37

---
+++ sudoku_solver_37.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        def is_valid(num: str, row: int, col: int) -> bool:
+            intervals = ((0, 3), (3, 6), (6, 9))
+            row_int, col_int = None, None
+            for interval in intervals:
+                if row >= interval[0] and row < interval[1]:
+                    row_int = interval
+
+                if col >= interval[0] and col < interval[1]:
+                    col_int = interval
+
+            for x in range(row_int[0], row_int[1]):
+                for y in range(col_int[0], col_int[1]):
+                    if board[x][y] == num:
+                        return False
+
+            for x in range(9):
+                if board[row][x] == num or board[x][col] == num:
+                    return False
+
+            return True
+
+        def backtrack() -> bool:
+            for row in range(9):
+                for col in range(9):
+                    if board[row][col] == ".":
+                        for num in "123456789":
+                            if is_valid(num, row, col):
+                                board[row][col] = num
+                                if backtrack():
+                                    return True
+                                board[row][col] = "."
+                        return False
+            return True
+
+        backtrack()
