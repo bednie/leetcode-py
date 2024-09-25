1bf6f4dc9e1adbaef828ad156a5a75f4de9a5568
Blair Ednie <bednie@msudenver.edu>
2024-09-24 21:41:16 -0600

    Solve 94

---
+++ binary_tree_inorder_94.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def helper(root: Optional[TreeNode]):
+            if not root:
+                return
+
+            helper(root.left)
+
+            self.vals.append(root.val)
+
+            helper(root.right)
+
+        self.vals = []
+        helper(root)
+        return self.vals
