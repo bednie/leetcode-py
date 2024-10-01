3cf43ed6f36eb16f2f18180f33fee0e3dd7c6cf6
Blair Ednie <bednie@msudenver.edu>
2024-09-30 23:00:41 -0600

    Work on 111

--- min_depth_binary_tree_111.py
+++ min_depth_binary_tree_111.py
@@ -11,17 +11,17 @@ class TreeNode:
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         def helper(root: Optional[TreeNode], depth: int):
-            if not root:
+            if not root or depth > self.result:
                 return
-
-            helper(root.left, depth + 1)
 
             if not root.left and not root.right:
                 self.result = min(self.result, depth + 1)
+                return
 
+            helper(root.left, depth + 1)
             helper(root.right, depth + 1)
             return
 
-        self.result = 10**5 if root else 0
+        self.result = 10_000 if root else 0
         helper(root, 0)
         return self.result
