e7838616f355f4a17d37d37067a6d0ac464855fc
Blair Ednie <bednie@msudenver.edu>
2024-09-30 19:58:08 -0600

    Solve 111

---
+++ min_depth_binary_tree_111.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        def helper(root: Optional[TreeNode], depth: int):
+            if not root:
+                return
+
+            helper(root.left, depth + 1)
+
+            if not root.left and not root.right:
+                self.result = min(self.result, depth + 1)
+
+            helper(root.right, depth + 1)
+            return
+
+        self.result = 10**5 if root else 0
+        helper(root, 0)
+        return self.result
